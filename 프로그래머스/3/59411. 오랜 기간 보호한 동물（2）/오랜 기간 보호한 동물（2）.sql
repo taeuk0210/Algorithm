de034aaf58ff7a5f9f8a36ea632b396c68b0029d
taeuk0210 <157875673+taeuk0210@users.noreply.github.com>
2024-11-25 16:00:30 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+SELECT
+    ao.ANIMAL_ID,
+    ao.NAME
+FROM 
+    ANIMAL_INS ai RIGHT JOIN ANIMAL_OUTS ao
+    ON ai.ANIMAL_ID = ao.ANIMAL_ID
+ORDER BY
+    ao.DATETIME - ai.DATETIME DESC
+LIMIT
+    2
