2a94d05b5bb9c6311da08aefdf6cccb70e51f789
taeuk0210 <157875673+taeuk0210@users.noreply.github.com>
2025-01-07 14:09:55 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.04 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int a, int b) {
+        b /= GCD(a, b);
+        while (b % 2 == 0) b /= 2;
+        while (b % 5 == 0) b /= 5;
+        return b == 1 ? 1 : 2;
+    }
+    
+    public int GCD(int a, int b) {
+         int t;
+        while (b!=0) {
+            t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+    
+}
