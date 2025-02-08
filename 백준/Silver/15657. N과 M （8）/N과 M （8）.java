809b3a7753c1b76543c7f5b1005a8b939fcf3924
taeuk0210 <157875673+taeuk0210@users.noreply.github.com>
2025-02-08 15:43:16 +0900

    [Silver III] Title: N과 M (8), Time: 340 ms, Memory: 34240 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static int n;
+    static int m;
+    static int[] nums;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(nums);
+        rec(0, 0, new int[m]);
+        System.out.println(sb);
+    }
+    
+    public static void rec(int s, int index, int[] input) {
+        if (index == m) {
+            sb.append(Arrays.toString(input)
+                    .replaceAll("[\\[\\],]",""))
+                    .append("\n");
+            return;
+        }
+        
+        for (int i=s; i<n; i++) {
+            input[index] = nums[i];
+            rec(i, index+1, input);
+            input[index] = 0;
+        }
+        
+    }
+    
+}
