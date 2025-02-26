1a43b8d7ce15806b180915d6f6bc72f7a58d642d
taeuk0210 <157875673+taeuk0210@users.noreply.github.com>
2025-02-26 18:15:42 +0900

    [Silver II] Title: 최소 힙, Time: 1372 ms, Memory: 110244 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,25 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m, n = sc.nextInt();
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++) {
+            m = sc.nextInt();
+            if (m == 0) {
+                if (queue.isEmpty()) {
+                    System.out.println("0");
+                } else {
+                    System.out.println(queue.poll());
+                }
+            } else {
+                queue.add(m);
+            }
+        }
+
+    }
+}
