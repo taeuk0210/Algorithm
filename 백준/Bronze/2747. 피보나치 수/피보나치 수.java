da9652b0b3c4e240063c877b7594760fcf50c3c0
taeuk0210 <157875673+taeuk0210@users.noreply.github.com>
2025-05-23 21:51:37 +0900

    [Bronze II] Title: 피보나치 수, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n <= 1) {
+            System.out.println(n);
+            return;
+        }
+        long f0 = 0;
+        long f1 = 1;
+        for (int i=0; i<n-1; i++) {
+            long t = f1;
+            f1 = f0 + f1;
+            f0 = t;
+        }
+        System.out.println(f1);
+    }
+}
